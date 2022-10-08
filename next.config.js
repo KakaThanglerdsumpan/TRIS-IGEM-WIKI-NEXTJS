a05835b0ec55dda1bfe9f43b4771236dc26f52a4
Thamognya Kodi <contact@thamognya.com>
2022-10-08 23:15:58 +0700

    pllsl work

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
-    reactStrictMode: true
+    reactStrictMode: true,
+    assetPrefix: isProd ? '/thailand-ris/' : ''
     /*
     experimental: {
         runtime: 'experimental-edge'
