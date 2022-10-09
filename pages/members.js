4b7713941adafa97e27fcf364ff39e5a251f95ac
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 02:14:51 +0700

    update

--- pages/members.js
+++ pages/members.js
@@ -17,6 +17,16 @@ function MembersPage({ children }) {
           </div>
           <div class = "circle"></div>
         </div>
+        <div class = "row">
+          <div class = "column">
+            <h3 class = "membertag">NAME</h3>
+            <h3 class = "membertag">TEAM/POSITION</h3>
+            <div class = "row">
+              <img src = "https://static.igem.wiki/teams/4314/wiki/tmp.png" width={400}/>
+              <p>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis gravida neque convallis a. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Faucibus nisl tincidunt eget nullam non nisi est sit. Mattis vulputate enim nulla aliquet. Feugiat pretium nibh ipsum consequat nisl vel pretium. Augue mauris augue neque gravida in. Mi proin sed libero enim sed faucibus turpis. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Sed felis eget velit aliquet sagittis id. Adipiscing elit ut aliquam purus sit amet luctus venenatis. Tincidunt dui ut ornare lectus sit amet. Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Mi eget mauris pharetra et ultrices neque ornare.',</p>
+            </div>
+          </div>
+        </div>
         
 
     </div>
