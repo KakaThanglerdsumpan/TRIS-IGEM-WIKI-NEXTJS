bbeb310a52001a95ff567ee4722b209e9c4f1f47
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 01:51:15 +0700

    update

--- pages/members.js
+++ pages/members.js
@@ -11,7 +11,13 @@ function MembersPage({ children }) {
             <img src = "https://static.igem.wiki/teams/4314/wiki/tmp.png"/>
             <h1 class = "header1">INTRODUCING THE TEAM</h1>
         </div>
-
+        <div class = "row">
+          <div class = "rounded-container">
+            <h1 class = "header1">TEAM MEMBERS</h1>
+          </div>
+          <div class = "circle"></div>
+        </div>
+        
 
     </div>
   )
