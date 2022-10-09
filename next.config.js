11f6d8df421ccefbd70e668b6d2651bfe0945d24
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 02:36:56 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
-    reactStrictMode: true,
-    assetPrefix: isProd ? '/thailand-ris/' : ''
-    /*
-    experimental: {
-        runtime: 'experimental-edge'
-    }
-    */
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
---
+++ pages/news.js
@@ -0,0 +1,5 @@
+function  NewsPage() {
+    return <h1>The NewsPage</h1>
+}
+
+export default NewsPage;
