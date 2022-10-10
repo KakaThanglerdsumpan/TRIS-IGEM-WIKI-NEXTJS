1f60b83bb77c7672f14bbfbf4788de79107f4264
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 13:38:38 +0700

    please work

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
