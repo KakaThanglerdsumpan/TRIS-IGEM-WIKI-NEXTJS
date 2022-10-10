b226c047d262e875129457651a4280cb36d426c9
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 12:10:06 +0700

    pleasework 2

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/name-of-repository' : '',
+}
+
+module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static.igem.wiki',
-        port: '',
-        pathname: '/teams/4314/wiki/**',
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'static.igem.wiki',
+      port: '',
+      pathname: '/teams/4314/wiki/**',
+    },
+  ],
+},
 }
 
 module.exports = nextConfig
+
