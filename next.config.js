6e9dde668dbc2d7b84dd894db68ddc62ac77483b
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 12:25:11 +0700

    testing with blank next-app

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/name-of-repository' : '',
-}
-
-module.exports = {
-  images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'static.igem.wiki',
-      port: '',
-      pathname: '/teams/4314/wiki/**',
-    },
-  ],
-},
+  swcMinify: true,
 }
 
 module.exports = nextConfig
-
