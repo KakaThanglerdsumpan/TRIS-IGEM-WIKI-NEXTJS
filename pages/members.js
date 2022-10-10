e8d7d86b8f8778eb744c951a70646eda733e1da1
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 22:21:33 +0700

    started collabs page

--- pages/members.js
+++ pages/members.js
@@ -6,11 +6,13 @@ function MembersPage({ children }) {
                 <h1 class = "title">MEMBERS</h1>
         </div>
         <div class = "body">
+          <div class = "row">
             <img src = "https://static.igem.wiki/teams/4314/wiki/tmp.png" width= {700} height={450}/>
             <h1 class = "header1">INTRODUCING THE TEAM</h1>
+          </div>
         </div>
         <div class = "row">
-          <div class = "rounded-container">
+          <div class = "rounded-container1">
             <h1 class = "header1">TEAM MEMBERS</h1>
           </div>
           <div class = "circle"></div>
