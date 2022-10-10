3016f31ebe1fee197ca0a457840b907cec22f21a
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-10 11:56:37 +0700

    new gitlab ci

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const HomePage = ({ children }) => {
                 adfsadf
                 asdfafasdfad
                 fadf
-                adfsadfdfadsfasdfasdfasdfasdfasdfasdfasdfasdfadsf
+                adfsadfdfadsdfasdfasdfasdfasdfadsf
                 asdfasdfasdfadfasdfadsfadfadsf
                 sdfa
                 sdfasdf
