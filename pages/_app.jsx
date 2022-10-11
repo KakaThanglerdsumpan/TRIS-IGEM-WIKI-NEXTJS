f4a193394479dc43520ce38b50e74e653f1b4de0
Thamognya Kodi <contact@thamognya.com>
2022-10-12 02:27:22 +0700

    update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import Main from '../components/layouts/main'
 import Navbar from '../components/navbar'
 import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
     return (
         <div>
             <ChakraProvider>
