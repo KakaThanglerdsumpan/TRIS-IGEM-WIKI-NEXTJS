38fdb8e126742585250900be12cca27419660899
Thamognya Kodi <contact@thamognya.com>
2022-10-12 02:18:25 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,17 +3,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     //assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
-    async redirects() {
-        return [
-            {
-                source: '/home',
-                destination: '/',
-                permanent: true
-            }
-        ]
-    },
-    reactStrictMode: true
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
+    reactStrictMode: false
     // assetPrefix: isProd ? '/thailand-ris/' : ''
     /*
     experimental: {
