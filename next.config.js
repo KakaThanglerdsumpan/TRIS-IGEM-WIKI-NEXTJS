5c5ac65a3aa55e2516c4d8a2f65d0b9e25e09735
Thamognya Kodi <contact@thamognya.com>
2022-10-12 02:32:21 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    future: { webpack5: true }
 }
 
 module.exports = nextConfig
