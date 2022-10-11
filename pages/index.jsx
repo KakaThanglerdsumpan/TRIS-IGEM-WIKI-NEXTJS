913a04c4a1938e14051e721b8ad7fcc733034d55
Thamognya Kodi <contact@thamognya.com>
2022-10-12 02:29:14 +0700

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
 import { ArrowDownIcon } from '@chakra-ui/icons'
-import type { NextPage } from 'next'
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 import HomeLarge from '../components/homelarge'
 import { Show, Hide } from '@chakra-ui/react'
 import HomeSmall from '../components/homesmall'
 
-const Home: NextPage = () => {
+const Home = () => {
     return (
         <div>
             <Show below="sm">
