75d320e8d5145885869b3e30198e05c7512f663a
Thamognya Kodi <contact@thamognya.com>
2022-10-12 02:26:10 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-// const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
-    reactStrictMode: false
-    // assetPrefix: isProd ? '/thailand-ris/' : ''
-    /*
-    experimental: {
-        runtime: 'experimental-edge'
-    }
-    */
+    reactStrictMode: true,
+    swcMinify: true
 }
 
 module.exports = nextConfig
