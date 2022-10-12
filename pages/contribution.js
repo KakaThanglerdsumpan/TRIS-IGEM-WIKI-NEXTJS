5f5ac2c3ab50ca266cf67a5ccea37339bf7bcee9
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-12 18:36:35 +0700

    update

--- pages/contribution.js
+++ pages/contribution.js
@@ -35,19 +35,38 @@ Finally, our peptide can also be utilized by future teams.
 
             <div class = "sizedbox40"/>
         <h1 class = "header1">
-        Characterizing existing parts BBa_J18932, BBa_K1223006, BBa_J18918
+        Characterizing existing parts BBa_K1223006 and BBa_J18918
 </h1>
         <div class = "sizedbox20"></div>
         <p class = "indented"> 
-        We contributed to future iGEM teams by completing an experimental characterization on the existing part 
-        <a href = "http://parts.igem.org/Part:BBa_J18932" class = "blue"> <u>BBa_J18932</u></a>
-        , 
+        We contributed to future iGEM teams by completing an experimental characterization on the existing part  
         <a href = "http://parts.igem.org/Part:BBa_K1223006" class = "blue"> <u>BBa_K1223006</u></a>
-        , and 
+        and 
         <a href = "http://parts.igem.org/Part:BBa_J18918" class = "blue"> <u>BBa_J18918</u></a>.
-            </p>
+        </p>
+        <div class = "sizedbox20"/>
+        <p class = "indented">
+            Characterization of  <a href = "http://parts.igem.org/Part:BBa_K1223006" class = "blue"> <u>BBa_K1223006</u></a> TRIS_iGEM team
+        </p>
+        <div class = "sizedbox20"/>
+        <p class = "indented">
+        <b>Background:</b><br/>
+Similarly to what the 2019 Madrid team proposed, this part holds potential to be used in other ways of separation. For our experiment, the Histidine tag was used as a part of purification through His column. Since Histidine contains polar properties, metal ions such as Ni2+ or Cu2+, will attract the histidine and allow for bonds to form. 
 
-            <div class = "sizedbox40"/>
+This attraction will form strong bonds that will cause the His-tags to form bonds with the linings on the side of the column, allowing for washing of surplus proteins, without losing our target protein. 
+        </p>
+        <div class = "sizedbox20"/>
+        <p class = "indented">
+        <b>Experiment:</b> <br/>
+For our experiment, we had to undergo sonication to remove our protein from the host cell itself. However, when sonication occurs, even though the host cell is destroyed and proteins are free, all of the proteins are merged together. Thus, we had to separate our target protein from the rest of the proteins that kept the cells functioning. To accomplish this, we had to depend upon the His-tags. Within the amino acids of our peptide sequence Histidine which contains polar properties. With this, we decided to use His-columns, columns that are lined on the edges with metal ions, to separate our target protein from the rest. The histidine within our protein sequence will be attracted to the metal ions on the sides of the column due to the bonds that will be formed between the Histidine and metal ions. Since the other protein doesn’t contain Histidine in its sequences, no bonding will occur which will cause it to continually float in the solution mixture, separating it from our target protein. As we wash away the solution mixture, none of our target protein will be removed since it's still bonded to the sides. Afterwards, we wash the column with imidazole, eluting the protein. The imidazole is capable of eluting the protein since it competes with the histidine to bind with the metal ions lining the his columns. 
+
+        </p>
+
+        
+
+
+        <div class = "sizedbox40"/>
+        
         <h1 class = "header1">Adding new parts to the iGEM registry</h1>
         <div class = "sizedbox20"></div>
         <p class = "indented"> 
