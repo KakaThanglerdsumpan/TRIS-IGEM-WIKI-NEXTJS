28a4cd9f672edc04401c270c349a162266139c8a
KakaThanglerdsumpan <dhiratht37@gmail.com>
2022-10-12 18:22:31 +0700

    added base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/thailand-ris',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/thailand-ris' : '',
   reactStrictMode: true,
   swcMinify: true,
